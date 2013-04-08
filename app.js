36c336879189cbca9c92639921a62c4b7f76f078
vieirar <raul.vieira@intelliware.ca>
2013-04-07 22:43:58 -0400

    routing to welcome on success

--- app.js
+++ app.js
@@ -5,20 +5,27 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , flash = require('connect-flash')
+  , passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -28,7 +35,40 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/welcome', routes.welcome);
+
+var users = {
+    'rottenjohny': 'password'
+};
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        console.log('Authenticating');
+        var pwd = users[username];
+        if(!pwd) {
+            return done(null, false, { message: 'Incorrect username' });
+        }
+        if(password !== pwd) {
+            return done(null, false, { message: 'Incorrect password' });
+        }
+        return done(null, { username: username });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    console.log('serializing user');
+    done(null, user.username);
+});
+
+passport.deserializeUser(function(user, done) {
+    console.log('deserializing user');
+    done(null, { username: user });
+});
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/welcome',
+                                                    failureRedirect: '/',
+                                                    failureFlash: true })
+);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', message: req.flash('error') });
 };
+
+exports.welcome = function(req, res) {
+    console.log('welcoming');
+    res.render('welcome', {username: req.user.username});
+}
